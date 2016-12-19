a687b566466b5320785c713c2d6c3231e32e752d
nsabusreeraj <nsabusreeraj@salesforce.com>
2016-12-19 17:19:06 +0000

    second commit

---
+++ CreditCardMask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardMask
+{
+    public static class Kata
+    {
+        public static void Main(string[] arg)
+        {
+            
+        }
+        // return masked string
+        public static string Maskify(string cc)
+        {
+            if (cc == null)
+                return null;
+            else if (cc.Length < 4)
+                return cc;
+            else
+            {
+
+                string subString = cc.Substring(cc.Length - 4);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i< cc.Length-4; i++)
+                    sb.Append("#");
+
+                Console.WriteLine(subString + ","+ sb+"->"+ (sb+subString));
+                return sb + subString;
+
+            }
+        }
+    }
+
+}
---
+++ CreditCardMaskTests/KataTests.cs
@@ -0,0 +1,17 @@
+﻿using CreditCardMask;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreditCardMaskTests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void MaskifyTest()
+        {
+            Assert.AreEqual(Kata.Maskify("4556364607935616"), "############5616");
+            Assert.AreEqual(Kata.Maskify("1"), "1");
+            Assert.AreEqual(Kata.Maskify("11111"), "#1111");
+        }
+    }
+}
